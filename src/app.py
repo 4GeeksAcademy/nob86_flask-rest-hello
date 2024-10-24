f0fab09b2447a14a10be12af6be8336811d9a13e
nob86 <gustavoadallacroce@gmail.com>
2024-10-24 05:57:00 +0000

    sigo mas tarde

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet
 #from models import Person
 
 app = Flask(__name__)
@@ -39,11 +39,13 @@ def sitemap():
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    planets = Planet.query.all()
 
-    return jsonify(response_body), 200
+    # response_body = {
+    #     "msg": "Hello, this is your GET /user response "
+    # }
+
+    return jsonify(planets), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
